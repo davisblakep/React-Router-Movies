d44bea3daf9138b4c657b42f6e94dc42be2d840b
Blake Davis <blakedavis@Blakes-MBP.lan1>
2020-05-06 20:42:35 -0400

    Added BrowserRouter as Router, included Route and Link items, included useParams to route IDs for each movie, and changed axios call to include id.id from useParams to call individual movie cards.  Project completed

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-
+import { Route } from 'react-router-dom';
+ 
 import SavedList from './Movies/SavedList';
+import MovieList from '../src/Movies/MovieList';
+import Movie from '../src/Movies/Movie';
 
 const App = () => {
   const [savedList, setSavedList] = useState([]);
@@ -28,7 +31,14 @@ const App = () => {
   return (
     <div>
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+      <div>
+        <Route exact path='/'>
+          <MovieList movies={movieList} />
+        </Route>
+        <Route path='/:id'>
+          <Movie />
+        </Route>
+      </div>
     </div>
   );
 };
